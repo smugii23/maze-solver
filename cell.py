f42acb74ed72d5de46f79d0b91ef43b5fb5fd2f3
smugii23 <smugii0203@gmail.com>
2025-02-12 16:30:28 -0800

    added a function to draw a line from one cell to another

---
+++ cell.py
@@ -0,0 +1,39 @@
+from graphics import Line, Point
+
+class Cell():
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.x1 = None
+        self.y1 = None
+        self.x2 = None
+        self.y2 = None
+        self.window = window
+
+    def draw(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self.window.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self.window.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self.window.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self.window.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        color = "gray" if undo else "red"
+        start = Point((self.x1 + self.x2) / 2, (self.y1 + self.y2) / 2)
+        end = Point((to_cell.x1 + to_cell.x2) / 2, (to_cell.y1 + to_cell.y2) / 2)
+        line = Line(start, end)
+        # Pass the color when drawing the line
+        self.window.draw_line(line, color)
--- graphics.py
+++ graphics.py
@@ -4,10 +4,10 @@ class Window():
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
-        self.__canvas.pack(expand=True, fill=BOTH)
-        self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(expand=1, fill=BOTH)
+        self.__running = False
     
     def redraw(self):
         self.__root.update_idletasks()
@@ -18,8 +18,23 @@ class Window():
         while self.__running:
             self.redraw()
     
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+    
     def close(self):
         self.__running = False
 
 
-        
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
