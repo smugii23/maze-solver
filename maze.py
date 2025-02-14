caafcd2f9a5b288a33aba077916bc2971bd34d30
smugii23 <smugii0203@gmail.com>
2025-02-14 15:31:59 -0800

    added a function to create a start and exit to maze

--- maze.py
+++ maze.py
@@ -21,20 +21,24 @@ class Maze():
         self.win = win
 
         self._create_cells()
+        self._break_entrance_and_exit() 
     
     def _create_cells(self):
-        self._cells = [[None] * self.num_rows for _ in range(self.num_cols)]
-        for i in range(self.num_rows):
-            for j in range(self.num_cols):
-                self._cells[j][i] = Cell(self.win)
+        for i in range(self.num_cols):
+            col_cells = []
+            for j in range(self.num_rows):
+                col_cells.append(Cell(self.win))
+            self._cells.append(col_cells)
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
-        x1 = self.x1 + (j * self.cell_size_x)
+        x1 = self.x1 + (i * self.cell_size_x)
         x2 = x1 + self.cell_size_x
-        y1 = self.y1 + (i * self.cell_size_y)
+        y1 = self.y1 + (j * self.cell_size_y)
         y2 = y1 + self.cell_size_y
-        self._cells[j][i].draw(x1, y1, x2, y2)
+        self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
     
     def _animate(self):
@@ -42,3 +46,9 @@ class Maze():
             return
         self.win.redraw()
         time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self.num_cols - 1][self.num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self.num_cols - 1, self.num_rows - 1)
